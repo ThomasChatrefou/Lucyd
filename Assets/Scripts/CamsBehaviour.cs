b17a5f707be20896790be97975b5a3d44ec358e4
ThomasChatrefou <91902256+ThomasChatrefou@users.noreply.github.com>
2021-12-07 22:02:12 +0100

    flexible camera

--- Assets/Scripts/CamsBehaviour.cs
+++ Assets/Scripts/CamsBehaviour.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class CamsBehaviour : MonoBehaviour
 {
-    private Vector3 offset;
+    public float timeOffset;
     public GameObject player;
+
+    private Vector3 constantOffset;
+    private Vector3 velocity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        constantOffset = transform.position - player.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        Vector3 offset = constantOffset + 2 * player.transform.forward;
+
+        //transform.position = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position,
+            player.transform.position + offset,
+            ref velocity,
+            timeOffset);
     }
 }
