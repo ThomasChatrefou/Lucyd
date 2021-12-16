f352502edf2b81a669dab741bd12e795ad502639
hmarseu <hmarseu@gaming.tech>
2021-12-16 07:06:10 +0100

    derniers ajouts

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    
+    public GameObject TerreMan;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            Instantiate(TerreMan, new Vector3(0.5f,0.8f, 15), new Quaternion(0, 0, 0, 0));
+               
             print("you finished it wp  bruhhh");
         }
     }
