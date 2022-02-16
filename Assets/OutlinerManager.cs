03a230930c71b2ba44d5f6d70d953424b95e19e3
Fouman42 <guillaume.jacot42@gmail.com>
2022-02-16 16:36:19 +0100

    init outline

---
+++ Assets/OutlinerManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlinerManager : MonoBehaviour
+{
+
+    private LayerMask mask;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("DarkWorldCam").GetComponent<Camera>();
+        GetComponent<Outline>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CustomMouseOver();
+    }
+
+    void CustomMouseOver()
+    {
+        if (GameManager.instance.darkWorld)
+            mask = LayerMask.GetMask("DarkWorld");
+        else
+            mask = LayerMask.GetMask("LightWorld");
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000f, mask))
+        {
+            if (hit.collider.GetComponent<Outline>())
+                hit.collider.gameObject.GetComponent<Outline>().enabled = true;
+            else if (hit.collider.gameObject != this)
+                GetComponent<Outline>().enabled = false;
+        }
+    }
+}
