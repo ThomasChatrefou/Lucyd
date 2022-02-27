1dc36c1e425db84eaefb07c775a1ecd23c93a07f
ThomasChatrefou <91902256+ThomasChatrefou@users.noreply.github.com>
2022-02-28 00:06:52 +0100

    button finished

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,54 +1,113 @@
+using System.Collections;
 using UnityEngine;
+
 
 public class Button : MonoBehaviour, IInteractable
 {
-    public bool on;
+    public bool On { get; set; }
 
-    [SerializeField] private float cooldown = 1f;
+    [SerializeField] private bool canBeDisabledByHitAgain = true;
+    [SerializeField] private bool hasTimer;
+    [SerializeField] private float switchOnDuration = 3f;
+    [SerializeField] private float antiSpamCooldown = 1f;
     [SerializeField] private string playerTag = "Player";
+    [SerializeField] private string animatorTrigger = "Pressed";
 
-    private float lastInteractionTime;
+    private bool _inRange;
+    private bool _hasInteracted;
+    private float _lastInteractionTime;
+    private Animator _animator;
+    private TimerHandler _timerBeforeSwitchOff;
+    private IEnumerator _timerRoutine;
+    private IController _characterController;
 
-    private bool inRange;
 
-    private void Update()
+    private void Awake()
     {
-        
+        _characterController = GameObject.Find("Player").GetComponent<IController>();
+        _animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        if (!hasTimer) return;
+        _timerBeforeSwitchOff = new TimerHandler();
+        _timerBeforeSwitchOff.Ended += SwitchOff;
+    }
 
+    private void OnDestroy()
+    {
+        if (hasTimer) 
+            _timerBeforeSwitchOff.Ended -= SwitchOff;
+    }
+    
     public void OnInteract()
     {
-        if(Time.time - lastInteractionTime < cooldown)
-        {
-            return;
-        }
+        if(Time.time - _lastInteractionTime < antiSpamCooldown) return;
+        _lastInteractionTime = Time.time;
 
-        lastInteractionTime = Time.time;
-
-        if (inRange)
+        if (_inRange)
         {
             Toggle();
+        }
+        else
+        {
+            _characterController.OnMove();
+            _hasInteracted = true;
         }
     }
 
     private void Toggle()
     {
-        if (on)
+        if (_animator) _animator.SetTrigger(animatorTrigger);
+        _hasInteracted = false;
+
+        if (On)
         {
-            on = false;
+            if (canBeDisabledByHitAgain)
+            {
+                SwitchOff();
+                if (hasTimer) 
+                    StopCoroutine(_timerRoutine);
+                return;
+            }
+
+            if (hasTimer) 
+                ResetTimer();
         }
         else
         {
-            on = true;
+            SwitchOn();
         }
+    }
+
+    private void ResetTimer()
+    {
+        _timerBeforeSwitchOff.Countdown = switchOnDuration;
+    }
+
+    public void SwitchOn()
+    {
+        On = true;
+
+        if (hasTimer)
+        {
+            _timerRoutine = _timerBeforeSwitchOff.Timer(switchOnDuration);
+            StartCoroutine(_timerRoutine);
+        }
+    }
+
+    public void SwitchOff()
+    {
+        On = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            inRange = true;
+            _inRange = true;
+            if (_hasInteracted) Toggle();
         }
     }
 
@@ -56,7 +115,7 @@ public class Button : MonoBehaviour, IInteractable
     {
         if (other.CompareTag(playerTag))
         {
-            inRange = false;
+            _inRange = false;
         }
     }
 }
