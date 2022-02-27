24368a22fafa1610b105438efba4070606b6ac3f
ThomasChatrefou <91902256+ThomasChatrefou@users.noreply.github.com>
2022-02-28 00:07:47 +0100

    Revert "button finished"
    This reverts commit 1dc36c1e425db84eaefb07c775a1ecd23c93a07f.

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,113 +1,54 @@
-using System.Collections;
 using UnityEngine;
-
 
 public class Button : MonoBehaviour, IInteractable
 {
-    public bool On { get; set; }
+    public bool on;
 
-    [SerializeField] private bool canBeDisabledByHitAgain = true;
-    [SerializeField] private bool hasTimer;
-    [SerializeField] private float switchOnDuration = 3f;
-    [SerializeField] private float antiSpamCooldown = 1f;
+    [SerializeField] private float cooldown = 1f;
     [SerializeField] private string playerTag = "Player";
-    [SerializeField] private string animatorTrigger = "Pressed";
 
-    private bool _inRange;
-    private bool _hasInteracted;
-    private float _lastInteractionTime;
-    private Animator _animator;
-    private TimerHandler _timerBeforeSwitchOff;
-    private IEnumerator _timerRoutine;
-    private IController _characterController;
+    private float lastInteractionTime;
 
+    private bool inRange;
 
-    private void Awake()
+    private void Update()
     {
-        _characterController = GameObject.Find("Player").GetComponent<IController>();
-        _animator = GetComponent<Animator>();
+        
     }
 
-    private void Start()
-    {
-        if (!hasTimer) return;
-        _timerBeforeSwitchOff = new TimerHandler();
-        _timerBeforeSwitchOff.Ended += SwitchOff;
-    }
 
-    private void OnDestroy()
-    {
-        if (hasTimer) 
-            _timerBeforeSwitchOff.Ended -= SwitchOff;
-    }
-    
     public void OnInteract()
     {
-        if(Time.time - _lastInteractionTime < antiSpamCooldown) return;
-        _lastInteractionTime = Time.time;
+        if(Time.time - lastInteractionTime < cooldown)
+        {
+            return;
+        }
 
-        if (_inRange)
+        lastInteractionTime = Time.time;
+
+        if (inRange)
         {
             Toggle();
-        }
-        else
-        {
-            _characterController.OnMove();
-            _hasInteracted = true;
         }
     }
 
     private void Toggle()
     {
-        if (_animator) _animator.SetTrigger(animatorTrigger);
-        _hasInteracted = false;
-
-        if (On)
+        if (on)
         {
-            if (canBeDisabledByHitAgain)
-            {
-                SwitchOff();
-                if (hasTimer) 
-                    StopCoroutine(_timerRoutine);
-                return;
-            }
-
-            if (hasTimer) 
-                ResetTimer();
+            on = false;
         }
         else
         {
-            SwitchOn();
+            on = true;
         }
-    }
-
-    private void ResetTimer()
-    {
-        _timerBeforeSwitchOff.Countdown = switchOnDuration;
-    }
-
-    public void SwitchOn()
-    {
-        On = true;
-
-        if (hasTimer)
-        {
-            _timerRoutine = _timerBeforeSwitchOff.Timer(switchOnDuration);
-            StartCoroutine(_timerRoutine);
-        }
-    }
-
-    public void SwitchOff()
-    {
-        On = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            _inRange = true;
-            if (_hasInteracted) Toggle();
+            inRange = true;
         }
     }
 
@@ -115,7 +56,7 @@ public class Button : MonoBehaviour, IInteractable
     {
         if (other.CompareTag(playerTag))
         {
-            _inRange = false;
+            inRange = false;
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,7 +43,7 @@ public class GameManager : MonoBehaviour
         if (Input.anyKey)
             resetLevelTimer = resetLevelCooldown;
 
-        if (sceneFader && resetLevelTimer < 0)
+        if (resetLevelTimer < 0)
             sceneFader.FadeTo(menuName);
 
         resetLevelTimer -= Time.deltaTime;
